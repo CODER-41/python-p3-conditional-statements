a3c0ac5679de2b9c63f626090758e7b2ca7f5799
RONNY <mboyaronny41@gmail.com>
2025-11-10 20:25:34 +0300

    control flow statements

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -2,16 +2,61 @@
 
 def admin_login(username, password):
     # your code here
-    pass
+    if password == "12345" and (username == "admin" or username == "ADMIN"):
+        return("Access granted")
+    else:
+        return("Access denied")
+
+
+
 
 def hows_the_weather(temperature):
     # your code here
-    pass
+    if temperature < 40:
+        return "It's brisk out there!"
+    
+    elif temperature >= 40 and temperature <= 65:
+        return "It's a little chilly out there!"
+    
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
+    
+#print(hows_the_weather(33))
+    
 
 def fizzbuzz(num):
     # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
+#print(fizzbuzz(3))
+    
 
 def calculator(operation, num1, num2):
     # your code here
-    pass
+    if operation == "+":
+        return num1 + num2
+    
+    elif operation == "-":
+        return num1 - num2
+    
+    elif operation == "*":
+        return num1 * num2
+    
+    elif operation == "/":
+        if num2 != 0:
+            return num1 / num2
+        else:
+            print("Error: Division by zero")
+            return None
+    else:
+        print("Invalid operation!")
+        return None
+#print(calculator("+", 5, 3))
